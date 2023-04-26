3c2efc944281492462f87e7dc7ae0d7f3cba9c1d
joshluca98 <joshluca98@gmail.com>
2023-04-26 13:39:04 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
