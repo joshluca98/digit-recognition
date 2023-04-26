afff3c070d9894276f05b4fc6a4a749c492de801
joshluca98 <joshluca98@gmail.com>
2023-04-26 14:43:09 -0700

    tested model for the first time with sample digits made in paint

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# mnist = tf.keras.datasets.mnist
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+# x_train = tf.keras.utils.normalize(x_train, axis=1)
+# x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 # model = tf.keras.models.Sequential()
 # model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
@@ -23,8 +23,26 @@ x_test = tf.keras.utils.normalize(x_test, axis=1)
 # model.save('handwritten.model')
 
 model = tf.keras.models.load_model('handwritten.model')
+print("Model loaded successfully")
 
-loss, accuracy = model.evaluate(x_test, y_test)
+# loss, accuracy = model.evaluate(x_test, y_test)
 
-print(loss)
-print(accuracy)
+# print(loss)
+# print(accuracy)
+
+image_number = 1
+while os.path.isfile(f"digits/digit{image_number}.png"):
+    try:
+        print("Entered while loop try block")
+        img = cv2.imread(f"digits/digit{image_number}.png")[:,:,0]
+        img = np.invert(np.array([img]))
+        print("Image read and inverted")
+        prediction = model.predict(img)
+        print("Image prediction made")
+        print(f"This digit is probably a {np.argmax(prediction)}")
+        plt.imshow(img[0], cmap=plt.cm.binary)
+        plt.show()
+    except:
+        print("Error!")
+    finally:
+        image_number += 1
