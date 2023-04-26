7ec48c61b742bc3d21160e3ce93c215f11665455
joshluca98 <joshluca98@gmail.com>
2023-04-26 14:12:44 -0700

    test model and output results

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-# mnist = tf.keras.datasets.mnist
-# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# x_train = tf.keras.utils.normalize(x_train, axis=1)
-# x_test = tf.keras.utils.normalize(x_test, axis=1)
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 # model = tf.keras.models.Sequential()
 # model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
@@ -23,3 +23,8 @@ import tensorflow as tf
 # model.save('handwritten.model')
 
 model = tf.keras.models.load_model('handwritten.model')
+
+loss, accuracy = model.evaluate(x_test, y_test)
+
+print(loss)
+print(accuracy)
