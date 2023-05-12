54da380ee69136f598ec187b585f0764f36e3ba4
joshluca98 <joshluca98@gmail.com>
2023-05-11 17:59:28 -0700

    "massive changes"

---
+++ generate_model.py
@@ -0,0 +1,84 @@
+import os
+import tensorflow as tf
+import absl.logging
+
+# hide unnecessary warnings in command line
+absl.logging.set_verbosity(absl.logging.ERROR)
+
+# load and normalize MNIST dataset
+def load_mnist():
+    try:
+        mnist = tf.keras.datasets.mnist
+        (trainX, trainY), (testX, testY) = mnist.load_data()
+        trainX = tf.keras.utils.normalize(trainX, axis=1)
+        testX = tf.keras.utils.normalize(testX, axis=1)
+        print("\nMNIST dataset imported successfully.\n")
+        return trainX, trainY, testX, testY
+    except:
+        print("Error: failed to import MNIST dataset. Aborting...")
+        exit()
+
+# define, compile, and fit model
+def build_model():
+    try:
+        model = tf.keras.models.Sequential()
+        model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+        model.add(tf.keras.layers.Dense(128, activation='relu'))
+        model.add(tf.keras.layers.Dense(128, activation='relu'))
+        model.add(tf.keras.layers.Dense(10, activation='softmax'))
+        model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+        model.fit(trainX, trainY, epochs=7)
+        print(f"\nModel built successfully.\n")
+        return model
+    except:
+        print("Error: failed to build model. Aborting...")
+        exit()
+
+# save model to the working directory allowing later use
+def save_model():
+    try:
+        model.save('generated-models\digit-analysis-model')
+        print("Model saved successfully.\n")
+    except:
+        print("Error: failed to save model to current directory. Aborting...")
+        exit()
+
+# load model from working directory to confirm successful save
+def load_model():
+    try:
+        model = tf.keras.models.load_model('generated-models\digit-analysis-model')
+        print("Model loaded successfully.\n")
+        return model
+    except:
+        print("Error: failed to load model. Aborting...")
+        exit()
+
+# evaluate model performance on test data
+def evaluate_model():
+    try:
+        loss, accuracy = model.evaluate(testX, testY)
+        print("\nModel evaluated successfully:\n")
+        print(f"    Model Accuracy: {accuracy}")
+        print(f"    Model Loss: {loss}\n")
+        return loss, accuracy
+    except:
+        print("Error: failed to evaluate model. Aborting...")
+        exit()
+
+if os.path.isdir("generated-models\digit-analysis-model"):
+    print("Model already exists. Using existing model.")
+else:
+    confirmNewModel = input("\nNo model found. Press ENTER to generate model, or type \"a\" to abort: ")
+    while confirmNewModel != "" and confirmNewModel != "a":
+            confirmNewModel = input("No model found. Press ENTER to generate model, or type \"a\" to abort: ")
+    if confirmNewModel == "a":
+        print("Aborting...")
+        exit()
+
+    print("\nGenerating new model based on presets.")
+    trainX, trainY, testX, testY = load_mnist()
+    model = build_model()
+    save_model()
+    model = load_model()
+    evaluate_model()
+    print("Script ran successfully. Model has been generated and saved to working directory.")
---
+++ use_model.py
@@ -0,0 +1,65 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from generate_model import *
+
+# load existing model or generate model if not found
+def this_load_model():
+    try:
+        model = tf.keras.models.load_model('generated-models\digit-analysis-model')
+        print("\nModel loaded successfully.\n")
+        return model
+    except:
+        print("Error: failed to load model. Aborting...")
+
+
+# determine directory of files to be inputted to the model
+def define_directory():
+    print("\nEnter directory name where test images are stored or press ENTER to use default directory.\n")
+    while True:
+        directory = input("Directory Name: ")
+        if directory == "a":
+            print("Aborting...")
+            exit()
+        elif directory == "":
+            directory = "test-digits"
+            print("\nUsing default \"test-digits\" directory. ", end="")
+            if os.path.isdir(directory):
+                print("Directory has been found.")
+                return directory
+            else:
+                print("Default directory is missing. Aborting..")
+                exit()
+        else:
+            if os.path.isdir(directory):
+                print(f"Using \"{directory}\" directory.")
+                return directory
+            else:
+                print("Invalid directory, try again or type \"a\" to abort.")
+    
+
+# takes images as input to the model and outputs a prediction
+def use_model():
+    directory = define_directory()
+    model = this_load_model()
+        
+    img_num = 0
+    while os.path.isfile(f"{directory}/digit{img_num}.png"):
+        try:
+            img = cv2.imread(f"test-digits/digit{img_num}.png")[:,:,0]
+            img = np.invert(np.array([img]))
+            print(f"\"digit{img_num}.png\" read successfully.")
+            prediction = model.predict(img)
+            print("Image prediction made")
+            print(f"Model Prediction: This digit is a {np.argmax(prediction)}.\n")
+            plt.imshow(img[0], cmap=plt.cm.binary)
+            plt.show()
+        except:
+            print("Error..moving on to next image.")
+        finally:
+            img_num += 1
+    print("All images have been processed.\n")
+
+use_model()
